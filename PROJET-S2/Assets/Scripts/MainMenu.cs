2fdbd399c1528628c150af78c794e45924267df2
BuildTools <unconfigured@null.spigotmc.org>
2021-04-21 18:14:13 +0200

    tout

---
+++ PROJET-S2/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Local()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
+
+
